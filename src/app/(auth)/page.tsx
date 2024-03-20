9e987bcdbfacedce0157711fb1fad5417cd35fdd
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-03-20 16:20:54 -0300

    feat: initial component has been create

---
+++ src/app/(auth)/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/organisms/LoginForm";
+
+export default function Home() {
+  return (
+    <div className="overflow-hidden">
+      <LoginForm />
+    </div>
+  );
+}
