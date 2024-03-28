6102791234485979b714f84d56dd035b271f8355
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-03-28 12:32:15 -0300

    feat: route from next auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,60 +1,52 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
+import { NextApiHandler } from 'next'; 
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 const nextAuthOptions: NextAuthOptions = {
-	session: { strategy: 'jwt' },
-	secret: process.env.NEXTAUTH_SECRET,
 	providers: [
 		CredentialsProvider({
 			name: 'credentials',
 			credentials: {
+				username: {
+					label: 'username',
+					type: 'text',
+				},
 				email: {
 					label: 'email',
-					type: 'text', 
+					type: 'text',
 				},
 				password: {
 					label: 'password',
-					type: 'password', 
+					type: 'password',
 				},
 			},
 
 			async authorize(credentials) {
-
 				const response = await fetch('https://reqres.in/api/login', {
 					method: 'POST',
 					headers: { 'Content-type': 'application/json' },
 					body: JSON.stringify({
+						username: credentials?.username,
 						email: credentials?.email,
 						password: credentials?.password,
 					}),
 				});
-                
 				const user = await response.json();
-                
 				if (user && response.ok) {
 					return user;
 				}
+
 				return null;
 			},
 		})
 	],
-	pages: { signIn: '/' },
-	callbacks: {
-		async jwt({ token, user }) {
-			user && (token.user = user);
-			return token;
-		},
-		async session({ session, token }) {
-			session = token.user as any;
-			return session;
-		},
+    
+	pages: {
+		signIn: '/',
+		error: '/login', 
 	},
 };
 
+const handler: NextApiHandler = NextAuth(nextAuthOptions);
 
-
-const handler = NextAuth(nextAuthOptions);
-
-export {
-	handler as GET, handler as POST, nextAuthOptions 
-};
+export default handler; 
