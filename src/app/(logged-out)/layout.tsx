30604ce4710dbd081eb76561f139886a25da3996
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-03-29 00:54:41 -0300

    feat: loggedOutRouter verification

--- src/app/(logged-out)/layout.tsx
+++ src/app/(logged-out)/layout.tsx
@@ -1,11 +1,22 @@
 import Image from 'next/image';
 import bgImage from '@/assets/images/login-bg.svg';
 import logo from '@/assets/images/Chronos.svg';
+import { getServerSession } from 'next-auth';
+import { nextAuthOptions } from '../api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+import { Toaster } from '@/components/ui/toaster';
 
-export default function LoggedOutLayout({ children }: React.PropsWithChildren): JSX.Element {
+
+async function LoggedOutLayout({ children }: React.PropsWithChildren) {
+	const session = await getServerSession(nextAuthOptions);
+
+	if (session) {
+		redirect('/dashboard');
+	}
+
 	return (
 		<main
-			className='w-full h-screen bg-black rounded flex overflow-hidden relative '
+			className='w-full flex h-screen bg-black rounded  overflow-hidden relative '
 		>
 			<Image
 				src={bgImage.src}
@@ -22,7 +33,11 @@ export default function LoggedOutLayout({ children }: React.PropsWithChildren): 
 				className='md:block hidden w-32 right-10 top-10 object-cover absolute'
 			/>
 			{children}
+			<Toaster />
+
 		</main>
 	);
 }
+
+export default LoggedOutLayout;
 
