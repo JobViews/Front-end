cded9483f4c5e9746c5e6e80bcd7da5f232a5ada
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-03-29 00:56:29 -0300

    feat: Dashboard component

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,9 +1,22 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
-export default function LoginPage(): JSX.Element {
+const Dashboard = () => {
+	const router = useRouter();
+
+	async function logout() {
+		await signOut({ redirect: false });
+		router.replace('/login');
+	}
 	return (
 		<main>
-			<h1>Dashboard</h1>
+			<h1 className='text-black'>Dashboard</h1>
+			<Button onClick={logout}>Sair</Button>
 		</main>
 	);
-}
+};
+
+export default Dashboard;
 
