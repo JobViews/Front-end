0f3ba21fbf191361d8b8ef9fb5f74b9fa162f976
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-04-04 18:09:55 -0300

    feat: loggedOutLayout has been modified structure

--- src/app/(logged-out)/layout.tsx
+++ src/app/(logged-out)/layout.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
-import bgImage from '@/assets/images/login-bg.svg';
-import logo from '@/assets/images/Chronos.svg';
+import bgImage from '@/assets/images/Chronos_logo_white.svg';
 import { getServerSession } from 'next-auth';
 import { nextAuthOptions } from '../api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
@@ -16,22 +15,18 @@ async function LoggedOutLayout({ children }: React.PropsWithChildren) {
 
 	return (
 		<main
-			className='w-full flex h-screen bg-black rounded  overflow-hidden relative '
+			className='w-full flex h-screen bg-gradient-to-br from-[#0c060cdc] via-[#791d76de] to-[#1b0d1b26] rounded  overflow-hidden relative '
 		>
-			<Image
-				src={bgImage.src}
-				alt=''
-				width={720}
-				height={900}
-				className='md:block hidden w-full object-cover'
-			/>
-			<Image
-				src={logo}
-				alt=''
-				width={200}
-				height={200}
-				className='md:block hidden w-32 right-10 top-10 object-cover absolute'
-			/>
+			<div className='w-full flex justify-center items-center'>
+				<Image
+					src={bgImage.src}
+					alt=''
+					width={100}
+					height={100}
+					className='md:block hidden w-72 object-cover bg-center'
+				/>
+			</div>
+		
 			{children}
 			<Toaster />
 
